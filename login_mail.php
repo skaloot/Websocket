1710193f7179958e0174b16d99f6c63392d49f7f
Skaloot <skaloot@gmail.com>
2016-06-30 08:28:44 +0800

    Email Notification

---
+++ login_mail.php
@@ -0,0 +1,13 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+date_default_timezone_set("Asia/Kuala_lumpur");
+ini_set("error_reporting", E_ALL);
+
+
+if(isset($_GET["username"]) && isset($_GET["app_id"])) {
+	$username = $_GET['username'];
+	if($_GET["app_id"] === "ladiesfotochat") {
+		echo file_get_contents("http://www.ladiesfoto.com/websocket/login_mail.php?username=".$username);
+	}
+}
