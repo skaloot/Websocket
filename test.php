2fc4c3e69f1f1bf0b824b828aa1efe9f27a21819
Skaloot <skaloot@gmail.com>
2016-08-29 08:15:56 +0800

    Loop Test

--- test.php
+++ test.php
@@ -1,30 +1,130 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+
+        <title>Javascript Loop Test (60K data)</title>
+   	</head>
+
+   	<body>
+
+		<div>
+			<h2>Javascript Loop Test (60K data)</h2>
+			<button type="button" onclick="start_test()">Start Test</button>
+			<br><br>
+			<div id="result"></div>
+		</div>
 
 
 
-<script type="text/javascript">
+		<script type="text/javascript" src="//localhost/kpj/jquery-1.7.1.min.js"></script>
+		<script type="text/javascript">
 
-var arr = [
-	{id:123, name: "skaloot"},
-	{id2:456, name2: "etty"},
-	{id3:789, name3: "nana"},
-];
 
-// var obj = {haha:"skaloot",hehe:"etty",huhu:"nana"};
+		function get_date(today) {
+		    var today = new Date();
+		    var y = today.getFullYear();
+		    var m = today.getMonth()+1;
+		    var d = today.getDate();
+		    var h = today.getHours();
+		    var mt = today.getMinutes();
+		    var s = today.getSeconds();
+		    var date = m + "-" + d + "-" + y + "-" + h + "-" + mt + "-" + s;
+		    return date;
+		}
 
-var arr3 = ["skaloot","etty","nana"];
 
-var arr2 = [];
-arr2['huhu'] = "skaloot";
-arr2.haha = "nana";
-delete arr2.haha;
+		function DateDiff(time1, time2) {
+		    var diffMS = time1 - time2;    
+		    var diffS = Math.floor(diffMS / 1000);
+		    var diffM = Math.floor(diffS / 60);
+		    var diffH = Math.floor(diffM / 60);
+		    var diffD = Math.floor(diffH / 24);
+		    diffS = diffS - (diffM * 60);
+		    diffM = diffM - (diffH * 60);
+		    diffH = diffH - (diffD * 24);
 
-var obj = {id:123, name: "skaloot"};
-delete arr;
+		    if(diffMS > 9 && diffMS < 99) {
+		    	diffMS = "0"+diffMS;
+		    }
+		    if(diffMS < 10) {
+		    	diffMS = "00"+diffMS;
+		    }
 
-// arr.splice(arr.indexOf("skaloot"),1);
-delete arr3[0];
-delete arr3[1];
+		    return diffH+":"+diffM+":"+diffS+":"+diffMS;
+		}
 
-console.log(arr3);
 
-</script>
+		var start_test = function() {
+			var start = new Date().getTime();
+			$.getJSON("//localhost/port/api", function(data){
+				var finish = new Date().getTime();
+				$("#result").html(null);
+				$("#result").append("Duration - "+DateDiff(finish, start));
+				$("#result").append("<br><br>");
+				start = new Date().getTime();
+				init_1(start,data);
+				init_2(start,data);
+				init_3(start,data);
+				init_4(start,data);
+			});
+		}
+
+
+		var init_1 = function(start,arr) {
+			for(var i=0, len=arr.length; i<len; i++) {
+				if(i==arr.length-1) {
+					var str1 = start;
+					var finish = new Date().getTime();
+					$("#result").append("For loop (length caching)");
+					$("#result").append("<br>");
+					$("#result").append("Duration - "+DateDiff(finish, start));
+					$("#result").append("<br><br>");
+				}
+			}
+		}
+
+		var init_2 = function(start,arr) {
+			for(var i=0; i<arr.length; i++) {
+				if(i==arr.length-1) {
+					var str1 = start;
+					var finish = new Date().getTime();
+					$("#result").append("For loop");
+					$("#result").append("<br>");
+					$("#result").append("Duration - "+DateDiff(finish, start));
+					$("#result").append("<br><br>");
+				}
+			}
+		}
+
+		var init_3 = function(start,arr) {
+			for(var i in arr) {
+				if(i==arr.length-1) {
+					var str1 = start;
+					var finish = new Date().getTime();
+					$("#result").append("For in loop");
+					$("#result").append("<br>");
+					$("#result").append("Duration - "+DateDiff(finish, start));
+					$("#result").append("<br><br>");
+				}
+			}
+		}
+
+		var init_4 = function(n,arr) {
+			start = new Date().getTime();
+			arr.forEach(function(i, idx) {
+				if(idx==arr.length-1) {
+					var str1 = start;
+					var finish = new Date().getTime();
+					$("#result").append("ForEach loop");
+					$("#result").append("<br>");
+					$("#result").append("Duration - "+DateDiff(finish, start));
+					$("#result").append("<br><br>");
+				}
+			});
+		}
+
+		</script>
+	</body>
+</html>
