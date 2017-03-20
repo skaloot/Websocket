9ef7970df922191260efb3472dd2436db4eca4f1
Skaloot <skaloot@gmail.com>
2017-03-20 08:15:27 +0800

    Update UI

--- index.php
+++ index.php
@@ -8,28 +8,37 @@
 
         <link href="font.css" rel="stylesheet"/>
         <style>
-        body { margin: 0;font-family: 'Raleway', sans-serif; font-size:1em; letter-spacing:0.02em;  }
+        html { -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box; }
+        *, *:before, *:after { -webkit-box-sizing: inherit; -moz-box-sizing: inherit; box-sizing: inherit; }
+        body { margin: 0;font-family: 'Raleway', sans-serif; font-size:1em; letter-spacing:0.02em; font-size: 1em; }
         * { padding:0px; margin:0px;outline: 0; }
         p { line-height:1.4; }
-        #content { padding:15px; background-color:#ddd; overflow-y: auto; position: absolute; padding-bottom:30px;
-                   border:1px solid #CCC; margin:10px; height: auto; bottom: 50px; top: 0px;left: 0px;right: 0px; }
-        #content #chat p { line-height: 1.6; margin-right:150px; }
-        #seen-typing { color:#999; position:absolute; bottom:70px; z-index:10; left:25px; font-size:0.9em; }
-        #new-message { color:#eee; background-color: #333; padding: 5px; cursor:pointer; position:absolute; bottom:70px; z-index:10; left:25px; font-size:0.8em; border-radius:3px; display:none; margin:auto 40%; }
-        #input-holder { margin-left: 10px;margin-right: 30px; position: absolute;bottom: 10px;width: auto;left: 0px;right: 0px; }
-        #input { border:1px solid #ccc; display: block; padding:10px; width:100%; font-size:1em; }
+        #body { width: 100%; height:100%; margin:0px; }
+        #content { background-color:#FFF; overflow-y: auto; position:absolute; margin:20px; height:auto; bottom:40px; top:0px;left: 220px; right:0px; padding-bottom:25px; }
+        #content .chat p { line-height: 1.6; }
+        #content .chat p:hover { background-color:#f5f5f5 }
+        #seen-typing { color:#999; position:absolute; bottom:50px; font-size:0.8em; }
+        #new-message { color:#eee; background-color: #333; padding: 5px; cursor:pointer; position:absolute; bottom:50px; z-index:10; left:50%; font-size:0.8em; border-radius:3px; display:none; margin:auto -50px;
+            width:100px; text-align:center; }
+        #input-holder { margin: 20px; width:auto; position:absolute; left:220px; bottom:0px; right:0px; }
+        #input { border:2px solid #ddd; display: block; padding:10px; font-size:1em; border-radius:5px; -webkit-border-radius:5px; -moz-border-radius:5px; transition:border 0.2s; width:100%;  }
+        #input:focus { border:2px solid #bbb; }
         #status { width:200px; display:block; float:left; margin-top:15px; }
         .client { color:#000; }
         .server { color:#888; }
         .time { color:#999; opacity: 0;margin-left: 15px;font-size: 1em; float:right; margin-right: 15px; }
         p:hover .time { opacity: 1; }
         #reconnect { padding:7px 10px; border:1px solid #ccc; border-radius:3px; background-color:#fff; font-size:1em; }
-        #bg_login { position: absolute;background-color: #eee;width:100%;height:100%;display:none; }
-        #login { position:absolute;top: 30%;text-align: center;width:100%;font-size: 1em;display:none; }
-        #username { padding:10px;font-size:1em;max-width:300px;width:100%;margin:auto; }
+        #bg_login { position: absolute; background-color:#303e4d; width:100%;height:100%;display:none; }
+        #login { position:absolute;top: 30%;text-align: center;width:100%;display:none; }
+        #username { padding:10px;font-size:1em;max-width:300px;width:100%;margin:auto; border-radius:3px; border:0px; }
         #wrapper { width:auto;margin:auto 40px auto 20px; }
-        #users { position:absolute;right:10px;top:10px;bottom:60px;width:160px;border-left:1px solid #aaa; background-color:#eee; }
-        .user { padding:5px 15px; }
+        #users-wrapper { position:absolute;left:0px;top:0px;bottom:0px;width:220px;background-color:#303e4d;color:#FFF; }
+        #users-title, #channels-title { font-size: 1em; padding:10px 18px; border-bottom:1px solid #45515f; margin-bottom:10px; background-color:#263442; }
+        #users, #channels { color:#ccc; }
+        #channels-title { display:none; }
+        .user, .channel { font-size:0.9em; padding:7px 18px; cursor:pointer; transition:background-color 0.2s; }
+        .user:hover, .channel:hover, .channel-now { background-color:#3f4e5f; color:#FFF; }
         @media only screen and (max-width: 400px) {
             #users { display: none; }
             #content #chat p { margin-right:20px; }
@@ -37,24 +46,33 @@
         </style>
     </head>
     <body>
-        <div id="content">
-            <div id="chat"></div>
-        </div>
-        <div id="users"></div>
-        <div id="seen-typing"></div>
-        <div id="new-message">New Message</div>
-        <div id="input-holder">
-            <input type="text" id="input" autocomplete="off" placeholder="Type here..">
-        </div>
-
-        <div id="bg_login"></div>
-        <div id="login">
-            <div id="wrapper">
-                <input id="username" autofocus autocomplete="off" placeholder="Your name please..">
+        <div id="body">
+            <div id="users-wrapper">
+                <div id="channels-title">Channels</div>
+                <div id="channels"></div>
+                <div id="users-title">Online Users</div>
+                <div id="users"></div>
             </div>
-        </div>
 
-        <input id="ip_address" type="hidden" value="<?php echo $_SERVER["REMOTE_ADDR"]; ?>">
+            <div id="content">
+                <div class="chat"></div>
+            </div>
+
+            <div id="input-holder">
+            <div id="seen-typing"></div>
+            <div id="new-message">New Message</div>
+                <input type="text" id="input" autocomplete="off" placeholder="Type here..">
+            </div>
+
+            <div id="bg_login"></div>
+            <div id="login">
+                <div id="wrapper">
+                    <input id="username" autofocus autocomplete="off" placeholder="Your name please..">
+                </div>
+            </div>
+
+            <input id="ip_address" type="hidden" value="<?php echo $_SERVER["REMOTE_ADDR"]; ?>">
+        </div>
 
         <script src="jquery-1.11.3.min.js"></script>
         <script src="client.js"></script>
