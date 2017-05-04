80823fcdb6f207154f91e773c9d853178f2c4f23
Tuan Mohd Shahril <skaloot@gmail.com>
2017-05-04 23:59:35 +0800

    test 3

--- webhook.php
+++ webhook.php
@@ -1,19 +1,19 @@
-<?php
+<?php (isset($_SERVER['HTTP_ACCEPT_ENCODING'])&&substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip'))?ob_start("ob_gzhandler"):ob_start(); 
 
 header('Access-Control-Allow-Origin: *');
 date_default_timezone_set("Asia/Kuala_lumpur");
 ini_set("error_reporting", E_ALL);
 
-require_once('db.php');
-$db = new Database();
-$db->connect();
+// require_once('db.php');
+// $db = new Database();
+// $db->connect();
 
 
 // if(isset($_POST["msg"])) {
-	$msg = json_encode($_POST);
-	$username = "Github";
-	$channel = "Webhook";
-	$ip_address = "";
+	// $msg = json_encode($_POST);
+	// $username = "Github";
+	// $channel = "Webhook";
+	// $ip_address = "";
 	// $db->insert("message", [
 	// 	"msg"=>$msg,
 	// 	"username"=>$username,
@@ -24,6 +24,7 @@ $db->connect();
 	$myFile = "github.log";
 	$fh = fopen($myFile, 'w') or die("can't open file");
 	$msg = json_encode($_POST);
+	// $msg = "hahaha";
 	fwrite($fh, $msg);
 	fclose($fh);
 // }
