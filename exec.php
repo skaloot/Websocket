1798c2dd9786df578320f89a2b12164a86427b33
Skaloot <skaloot@gmail.com>
2018-01-07 01:12:18 +0800

    Cleanup

--- exec.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$path = getcwd();
-
-if(strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-	echo rtrim(shell_exec('"C:/Program Files/nodejs/node.exe" '.$path.'/cli.js 2>&1').PHP_EOL);
-} else {
-	echo rtrim(shell_exec('/usr/local/bin/node '.$path.'/cli.js 2>&1').PHP_EOL);
-}
