7364c3c30fc67d0af60ecc8cda41a981396b828e
Skaloot <skaloot@gmail.com>
2018-06-05 08:03:51 +0800

    http

--- http.js
+++ http.js
@@ -1,10 +1,10 @@
 var http = require("http");
 
 http.createServer(function(request, response) {
+	console.log("Incoming cobbection..");
 	response.writeHead(200, {"Content-Type": "text/plain"});
-	console.log(request);
 	response.write("Hello Ska");
 	response.end();
-}).listen(3000);
+}).listen(8080);
 
 // hahaha
